2507c74a398d4be6c4e080fdae09fcb4ece8b589
Raja Shekar <raja.sj@pathpartnertech.com>
2018-07-23 10:39:44 +0530

    edited main function by adding mod mul functions

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 int sum(int , int);
+int mul(int , int);
+int mod(int ,int);
+
 int main()
 {
 	int a,b;
 	puts("enter values of a b");
 	scanf("%d%d",&a,&b);
 	printf("sum = %d\n",sum(a,b));
+	printf("mul = %d\n",mul(a,b));
+	printf("rem = %d\n",mod(a,b));
 	return 0;
 }
